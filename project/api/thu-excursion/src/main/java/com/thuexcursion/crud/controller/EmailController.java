a4d81ab2c488d3effe1ea75037cdb7654530d28c
charissaabegail <morales@mail.hs-ulm.de>
2021-11-25 09:53:40 +0100

    feat: mail functionality for 1 recipient

---
+++ project/api/thu-excursion/src/main/java/com/thuexcursion/crud/controller/EmailController.java
@@ -0,0 +1,41 @@
+package com.thuexcursion.crud.controller;
+
+
+import com.thuexcursion.crud.model.Mail;
+import com.thuexcursion.crud.service.SendMailService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import javax.mail.MessagingException;
+
+@RestController
+@RequestMapping("/api/v1/mail/")
+public class EmailController {
+    SendMailService service;
+
+    public EmailController(SendMailService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendMail(@RequestBody Mail mail) {
+        service.sendMail(mail);
+        return new ResponseEntity<>("Email Sent successfully", HttpStatus.OK);
+    }
+
+	@PostMapping("/sendtolist")
+    public ResponseEntity<String> sendMailToList(@RequestBody List<Mail> mail) {
+        service.sendMailToList(mail);
+        return new ResponseEntity<>("Email Sent successfully", HttpStatus.OK);
+    }
+
+    @PostMapping("/attachment")
+    public ResponseEntity<String> sendAttachmentEmail(@RequestBody Mail mail) throws MessagingException {
+        service.sendMailWithAttachments(mail);
+        return new ResponseEntity<>("Attachment mail sent successfully", HttpStatus.OK);
+    }
+}
