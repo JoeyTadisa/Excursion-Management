2658a25f56645c063e15b5a3959766cccbcb7c44
uelola <80069286+uelola@users.noreply.github.com>
2021-12-07 17:07:52 +0100

    approval on webui

--- project/web/frontend/src/LoggedInView.js
+++ project/web/frontend/src/LoggedInView.js
@@ -3,7 +3,6 @@ import "./App.css";
 import SubmitButton from "./SubmitButton";
 import UserStore from "./stores/UserStore";
 import ExcursionList from "./Excursions/ExcursionList";
-import ExcursionsList from "./Excursions/ExcursionsList";
 import NewExcursionEntry from "./NewExcursionEntry";
 import ExcursionDetailedItem from "./Excursions/ExcursionDetailedItem";
 import ExcursionsDetailedList from "./Excursions/ExcursionsDetailedList";
@@ -32,7 +31,10 @@ import ExcursionsDetailedList from "./Excursions/ExcursionsDetailedList";
 const LoggedInView = () => {
   const [excursions, setExcursions] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [notApprovedExcursions, setNotApprovedExcursions] = useState([]);
+  const [isLoading1, setIsLoading1] = useState(false);
   const [error, setError] = useState(null);
+  const [error1, setError1] = useState(null);
 
   // once loggedin the excursion list is set to be visible
   const [state, setState] = useState({
@@ -45,7 +47,9 @@ const LoggedInView = () => {
     // clear previous error
     setError(null);
     try {
-      const response = await fetch("http://localhost:9191/excursions");
+      const response = await fetch(
+        "http://localhost:9191/approvedExcursions/true"
+      );
 
       // need to check before parsing the response body
       // signals if the response was successful
@@ -57,13 +61,15 @@ const LoggedInView = () => {
       const transformedExcursions = data.map((excursionData) => {
         return {
           id: excursionData.id,
-          //excursion_date: excursionData.excursion_date,
+          title: excursionData.title,
+          date_of_excursion: new Date(excursionData.date_of_excursion),
+          excursion_fee: excursionData.excursion_fee,
           description: excursionData.description,
           max_participants: excursionData.max_participants,
+          destination: excursionData.destination,
           reg_deadline: new Date(excursionData.reg_deadline),
           dereg_deadline: new Date(excursionData.dereg_deadline),
           meeting_details: excursionData.meeting_details,
-          title: excursionData.title,
         };
       });
       setExcursions(transformedExcursions);
@@ -89,17 +95,56 @@ const LoggedInView = () => {
   // need to use prev in order to receive the latest state snapshot
   // clean way to update a state when it is based on older snapshot of the same state
 
-  const addExcursionHandler = (excursion) => {
-    console.log(excursion);
-    setExcursions((prevExcursions) => {
-      return [excursion, ...prevExcursions];
-    });
-  };
-  // the function that is triggered when 'Back to Excursions' button
-  // is clicked in the 'ExcursionForm' that brings back the excursion list view
-  const BackToExcursionViewHandler = () => {
-    setState({ whichComponentToShow: "ExcursionList" });
-  };
+  // fetch all not approved excursions from backend and display
+  const fetchNotApprovedExcursions = useCallback(async () => {
+    setIsLoading1(true);
+    // clear previous error
+    setError1(null);
+    try {
+      const response = await fetch(
+        "http://localhost:9191/approvedExcursions/false"
+      );
+
+      // need to check before parsing the response body
+      // signals if the response was successful
+      if (!response.ok) {
+        throw new Error("*** Something went wrong! ***");
+      }
+      const data = await response.json();
+
+      const transformedNewExcursions = data.map((newExcursionData) => {
+        return {
+          id: newExcursionData.id,
+          title: newExcursionData.title,
+          date_of_excursion: new Date(newExcursionData.date_of_excursion),
+          excursion_fee: newExcursionData.excursion_fee,
+          description: newExcursionData.description,
+          max_participants: newExcursionData.max_participants,
+          destination: newExcursionData.destination,
+          reg_deadline: new Date(newExcursionData.reg_deadline),
+          dereg_deadline: new Date(newExcursionData.dereg_deadline),
+          meeting_details: newExcursionData.meeting_details,
+        };
+      });
+      setNotApprovedExcursions(transformedNewExcursions);
+      console.log(notApprovedExcursions);
+    } catch (error) {
+      setError1(error.message);
+    }
+    // done loading, no matter if we got successful or an error response
+    setIsLoading1(false);
+  }, []);
+
+  /* useEffect is needed to load the content of the existing excursions on the load of LogedInView
+     don't need to be called each time it is re-evaluated (hence, avoid infinite task)
+     therefore, add 2nd argument, array of dependencies where we define when it should be executed
+     will only execute again if the dependencies [] listed below change
+     NEED TO BE CALLED AFTER fetchExcursions function !!!!!!!!!
+   */
+  useEffect(() => {
+    fetchNotApprovedExcursions();
+  }, [fetchNotApprovedExcursions]);
+
   // for better user experience need to display to user
   // if the content is loading/error occured
   let content = <p>Found no excursions.</p>;
@@ -113,11 +158,27 @@ const LoggedInView = () => {
     content = <p>Loading...</p>;
   }
 
-  //render() {
-  //user is loggedin, the excursion list & logout button is visible, greet the user by the name
+  let content1 = <p>No excursions pending for approval.</p>;
+  if (notApprovedExcursions.length > 0) {
+    content1 = <ExcursionList items={notApprovedExcursions} />;
+  }
+  if (error1) {
+    content1 = <p>{error1}</p>;
+  }
+  if (isLoading1) {
+    content1 = <p>Loading...</p>;
+  }
+
+  // the function that is triggered when 'Back to Excursions' button
+  // is clicked in the 'ExcursionForm' that brings back the excursion list view
+  const BackToExcursionViewHandler = () => {
+    setState({ whichComponentToShow: "ExcursionList" });
+  };
+
+  //organiser is loggedin, the excursion list & logout button is visible, greet the user by the name
   if (
     state.whichComponentToShow === "ExcursionList" &&
-    UserStore.name_last == "Crawford"
+    UserStore.user_type === "o"
   ) {
     return (
       <div className="loggedInView">
@@ -144,28 +205,17 @@ const LoggedInView = () => {
               onClick={() => this.doLogout()}
             />
             <br />
-
+            <h4 className="pending-for-approval">Pending for approval</h4>
+            <section className="pending-for-approval-box">{content1}</section>
             <h4>Available Excursions</h4>
-
             <section>{content}</section>
-            {/* <NewExcursionEntry onAddExcursion={addExcursionHandler} />*/}
-            {/*{!isLoading && excursions.lenght > 0 && (
-              <ExcursionList items={excursions} />
-            )}
-            {!isLoading && excursions.lenght === 0 && !error && (
-              <p>Found no movies</p>
-            )}
-            {!isLoading && error && <p>{error}</p>}
-            {isLoading && <p>Loading...</p>} */}
-            <ExcursionsDetailedList items={excursions} />
           </div>
         </div>
       </div>
     );
-
-    //Excursion form will be displayed with logout button
+    // Excursion form will be displayed with logout button for organiser
   } else if (
-    UserStore.name_last === "Crawford" &&
+    UserStore.user_type === "o" &&
     state.whichComponentToShow === "NewExcursionEntry"
   ) {
     return (
@@ -173,22 +223,22 @@ const LoggedInView = () => {
         <div className="app">
           <div className="container">
             <NewExcursionEntry
-              onAddExcursion={addExcursionHandler}
               onBackToExcursionView={BackToExcursionViewHandler}
             />
           </div>
         </div>
       </div>
     );
+    //
   } else if (
-    UserStore.name_last === "Scroggins" &&
+    UserStore.user_type === "a" &&
     state.whichComponentToShow === "ExcursionList"
   ) {
     return (
       <div className="loggedInView">
         <div className="app">
           <div className="container">
-            {/* welcome the user with the name of the user*/}
+            {/* welcome the admin with the name */}
             <h2>
               Welcome {UserStore.name_first} {UserStore.name_last}!
             </h2>
@@ -200,31 +250,16 @@ const LoggedInView = () => {
               onClick={() => this.doLogout()}
             />
             <br />
-
+            {/* show excursions pending for approval */}
+            <h4 className="pending-for-approval">Pending for approval</h4>
+            <section className="pending-for-approval-box">{content1}</section>
             <h4>Available Excursions</h4>
-
-            <button className="btn btn-primary" onClick={fetchExcursions}>
-              Fetch Excursions
-            </button>
             <section>{content}</section>
-            {/* <NewExcursionEntry onAddExcursion={addExcursionHandler} />*/}
-            {/*{!isLoading && excursions.lenght > 0 && (
-              <ExcursionList items={excursions} />
-            )}
-            {!isLoading && excursions.lenght === 0 && !error && (
-              <p>Found no movies</p>
-            )}
-            {!isLoading && error && <p>{error}</p>}
-            {isLoading && <p>Loading...</p>} */}
-            <ExcursionsDetailedList items={excursions} />
           </div>
         </div>
       </div>
     );
-
-    //Excursion form will be displayed with logout button
   }
 };
-//} if use render
 
 export default LoggedInView;
