303b65048884ca85b31b69f8d1ef21f549ec784e
charissaabegail <charissamorales1723@gmail.com>
2022-01-19 23:51:54 +0100

    docu: added documentation on AuthController and BookingController

--- project/api/thu-excursion/src/main/java/com/thuexcursion/crud/controller/BookingController.java
+++ project/api/thu-excursion/src/main/java/com/thuexcursion/crud/controller/BookingController.java
@@ -33,8 +33,9 @@ public class BookingController {
 	
 	
 	/** 
-	 * @param booking
-	 * @return Booking
+	 * This method is used to book an excursion. 
+	 * @param booking a booking object passed in the requestBody
+	 * @return Booking newly booked excursion
 	 */
 	@PostMapping("/bookAnExcursion")
 	public Booking addBooking(@RequestBody Booking booking) {
@@ -43,8 +44,9 @@ public class BookingController {
 
 	
 	/** 
-	 * @param bookings
-	 * @return List<Booking>
+	 * This method is used to book list of excursions. 
+	 * @param List<bookings>
+	 * @return List<Booking> newly booked list of excursions
 	 */
 	@PostMapping("/bookExcursions")
 	public List<Booking> addBookings(@RequestBody List<Booking> bookings) {	
@@ -53,7 +55,8 @@ public class BookingController {
 
 	
 	/** 
-	 * @return List<Booking>
+	 * This method fetch all bookings from the database
+	 * @return List<Booking> list of all bookings
 	 */
 	@GetMapping("/bookings")
 	public List<Booking> findAllbBookings() {
@@ -62,8 +65,8 @@ public class BookingController {
 
 	
 	/** 
-	 * 
-	 * @param matriculation_number represents the matriculation number of a student
+	 * This method fetch all bookings per student id.
+	 * @param matriculation_number represents the matriculation number of a student. This is user_id in user table with user_type of ROLE_S
 	 * @return List<StudentBookingExcursion> returns list of excursions booked by a student
 	 */
 	
@@ -75,7 +78,8 @@ public class BookingController {
 
 	
 	/** 
-	 * @param id
+	 * 
+	 * @param id id 
 	 * @return Booking
 	 */
 	@GetMapping("/booking/{id}")
